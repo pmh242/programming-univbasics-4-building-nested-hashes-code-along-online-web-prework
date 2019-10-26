835a4c04f135905cff34cc65bba84803f96a6fac
Pedro Huertas <pmh242@gmail.com>
2019-10-26 08:51:24 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,7 @@ def bonus
 
   #code your solution here:
   
-  epic_tragedy[:montague][:hero][:status]= "dead",
-  
+  epic_tragedy[:montague][:hero][:status]="dead"
   epic_tragedy[:capulet][:heroine][:status]="dead"
   
 
